607e6bf0e217b59c449ed89c1c44d37711c93f02
Shea Faigan <stfaigan@gmail.com>
2022-10-04 20:51:47 -0700

    v1

---
+++ script.js
@@ -0,0 +1,137 @@
+function setRaffleInfo(inProgress = false, numTickets = 0, drawn = []) {
+  localStorage.setItem("inProgress", inProgress);
+  localStorage.setItem("numTickets", numTickets);
+  localStorage.setItem("drawn", JSON.stringify(drawn));
+}
+
+function getRaffleInfo(key) {
+  switch (key) {
+    case "inProgress":
+      return localStorage.getItem("inProgress") === "true";
+    case "numTickets":
+      return parseInt(localStorage.getItem("numTickets"));
+    case "drawn":
+      return JSON.parse(localStorage.getItem("drawn"));
+  }
+}
+
+function saveDrawnNumber(number) {
+  var numbersDrawn = getRaffleInfo("drawn");
+  numbersDrawn.push(number);
+  localStorage.setItem("drawn", JSON.stringify(numbersDrawn));
+}
+
+function generateRaffle() {
+  var inProgress = getRaffleInfo("inProgress");
+  if (inProgress) {
+    var numTickets = getRaffleInfo("numTickets");
+    var drawn = getRaffleInfo("drawn");
+    var container = document.getElementById("grid");
+    container.replaceChildren();
+    var children = [];
+    for (var i = 1; i <= numTickets; i++) {
+      var gridCell = document.createElement("div");
+      gridCell.className = drawn.includes(i) ? "grid-cell drawn" : "grid-cell"
+      gridCell.append(i.toString());
+      container.append(gridCell);
+    }
+    var raffleMenu = document.getElementById("raffle-menu");
+    raffleMenu.className = "menu";
+    toggleStartMenu.call(document.getElementById("toggle-start-menu"));
+  }
+}
+
+function handleGenerateRaffle() {
+  var inProgress = getRaffleInfo("inProgress");
+  if (inProgress && !confirm("Raffle in progress, are you sure you want to restart?")) {
+    return false;
+  }
+  var numTickets = document.getElementById("number-tickets").value;
+  setRaffleInfo(true, numTickets)
+  generateRaffle();
+  updateTicketCount();
+  return false;
+}
+
+function toggleStartMenu() {
+  this.classList.toggle("active");
+  var panel = this.nextElementSibling;
+  if (panel.style.maxHeight) {
+    panel.style.maxHeight = null;
+  } else {
+    panel.style.maxHeight = panel.scrollHeight + "px";
+  }
+}
+
+function updateTicketCount() {
+  var numTickets = getRaffleInfo("numTickets");
+  var numDrawn = getRaffleInfo("drawn").length;
+  var numRemaining = numTickets - numDrawn;
+  var elem = document.getElementById("ticket-count");
+  elem.innerText = `Tickets Remaining: ${numRemaining}/${numTickets}`;
+}
+
+function handleDrawNumber() {
+  var numTickets = getRaffleInfo("numTickets");
+  var drawnNumbers = getRaffleInfo("drawn");
+  if (drawnNumbers.length >= numTickets) {
+    alert("No tickets left to draw!");
+    return;
+  }
+  var remainingNumbers = [];
+  for (var i = 1; i <= numTickets; i++) {
+    if (!drawnNumbers.includes(i))
+      remainingNumbers.push(i);
+  }
+  var number = remainingNumbers[Math.floor(Math.random()*remainingNumbers.length)];
+  saveDrawnNumber(number)
+
+  updateTicketCount();
+  var numberDrawn = document.getElementById("number-drawn");
+  numberDrawn.innerText = "Last Number Drawn: " + number.toString();
+
+  var popup = document.getElementById("number-drawn-popup");
+  popup.innerText = number.toString();
+  popup.classList.remove("zero-out");
+  popup.classList.add("opaque");
+  var drawNumberBtn = document.getElementById("draw-number");
+  drawNumberBtn.toggleAttribute("disabled");
+  setTimeout(function() {
+    popup.addEventListener("transitionend", function() {
+      drawNumberBtn.toggleAttribute("disabled");
+      popup.classList.add("zero-out");
+      popup.innerText = "";
+      console.log("end");
+    }, { once: true });
+    popup.classList.toggle("opaque");
+  }, 2000);
+
+  var cell = document.querySelector(`#grid :nth-child(${number})`);
+  cell.classList.toggle("drawn");
+}
+
+window.onload = function onLoad() {
+  // Add toggleable start menu
+  var startMenuBtn = document.getElementById("toggle-start-menu");
+  startMenuBtn.addEventListener("click", toggleStartMenu);
+
+  // Prevent form from refreshing page
+  var form = document.getElementById("start-form");
+  function handleForm(event) { event.preventDefault(); } 
+  form.addEventListener('submit', handleForm);
+
+  var raffleMenu = document.getElementById("raffle-menu");
+
+  if (getRaffleInfo("inProgress")) {
+    // Resume existing raffle
+    generateRaffle();
+    toggleStartMenu.call(startMenuBtn);
+    raffleMenu.className = "menu";
+    updateTicketCount();
+  } else {
+    // Set up for new raffle
+    setRaffleInfo()
+    toggleStartMenu.call(startMenuBtn);
+    raffleMenu.className = "menu hidden";
+  }
+}
